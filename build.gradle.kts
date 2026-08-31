f4570974d2f0437e764cf368198cca955584663d
technoir <technoir.dev@gmail.com>
2026-08-31 18:34:05 +0100

    Aggregate API reference

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
 }
 
 dependencies {
+    dokka(project(":cmake-import-gradle-plugin"))
+    dokka(project(":vfs-overlay-gradle-plugin"))
+
     nmcpAggregation(project(":cmake-import-gradle-plugin"))
     nmcpAggregation(project(":vfs-overlay-gradle-plugin"))
 }
